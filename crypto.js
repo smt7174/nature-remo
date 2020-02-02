cc9fd4af5b59957c6e24012044684db10ec29cb9
smt7174 <suzukima@belltreesoft.jp>
2020-02-02 11:42:31 +0900

    crypto.js変えた

--- crypto.js
+++ crypto.js
@@ -2,15 +2,15 @@ const cryptoJs = require("crypto-js");
 
 module.exports.getDecriptedAccessToken = () => {
 
-    const salt = 'SrdPayDEN1x4Jw9YYM2V';
-    // const planeText = 'ejFZm2306Qct5Q-n7oLH3waqsewKB1K03b1VPZ8RYSw.dmDN3aVZz0XrYgiJvquMr0UxZ1xliEd5pb5VSjSHFzA';
+    const salt = '';
+    // const planeText = '';
 
     // console.log('暗号化するテキスト : ' + planeText);
     // console.log('暗号化キー        : ' + salt);
 
     // 暗号化
     // const encryptedTextdText = cryptoJs.AES.encrypt(planeText, salt).toString();
-    const encryptedTextdText = "U2FsdGVkX1/Og05qN2hGJ/7qX/Y1f9sfjufrULiCbun93qhANdzc+DFgptNYuZk+4OB4hAFle9iJP0v73ROqRpSpHQ2kxMEP7RdCjuMEpOBWXUSXv3PjmtkYLt7mVSi+m+ysBxVIHLD/W6u/WbzcCQ==";
+    const encryptedTextdText = "";
     // console.log('暗号化(AES) : ' + encryptedTextdText);
 
     // 復号
