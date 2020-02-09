801a001dc620b290badc5b812fb12a056969347f
Masaki Suzuki <suzukima@belltreesoft.jp>
2020-02-09 10:09:41 +0900

    crypto_sample.jsを追加

---
+++ crypto_sample.js
@@ -0,0 +1,29 @@
+const cryptoJs = require("crypto-js");
+
+/*
+ @sample of returning decrypted value.
+*/
+
+module.exports.getDecryptedAccessToken = () => {
+
+    // set your original phrese for encryption and decription to salt.
+    const salt = 'your-salt-phrase';
+
+    // encryption.
+    // if you don't know encrypted value, you can confirm it to uncomment and execute sources of line 12 and 13.
+    // (Encrypted value is displayed at your console as a log)
+    // set password value you want to encrypt and decript to planeText.
+    // const planeText = 'your-original-password-value';
+    // console.log(`[encryptedText] ${cryptoJs.AES.encrypt(planeText, salt).toString()}`)
+
+    // decription
+    // set encrypted value to encryptedText
+    const encryptedText = "encrypted-value";
+
+    // the decrypted value(=unencrypted one) is set to decryptedText.
+    const decryptedText = cryptoJs.AES.decrypt(encryptedText, salt).toString(cryptoJs.enc.Utf8);
+    // console.log(`[decryptedText] ${decryptedText}`);
+
+    return decryptedText;
+
+};
